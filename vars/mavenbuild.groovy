516bc7381170a97568e09fe06a4c40c3de2dd72d
ujnvdprasad <124246569+ujnvdprasad@users.noreply.github.com>
2023-11-07 18:12:33 +0530

    Update mavenbuild.groovy

--- vars/mavenbuild.groovy
+++ vars/mavenbuild.groovy
@@ -1,18 +1,18 @@
 def call(String MavenCommand) {
-def MavenCommand = tool name: 'Maven-3.9.5', type: 'maven'
+def Maven = tool name: 'Maven-3.9.5', type: 'maven'
   if ("${MavenCommand}" == "Clean" ) {
-    sh "${MavenCommand}/bin/mvn clean"
+    sh "${Maven}/bin/mvn clean"
   }
   else if ("${MavenCommand}" == "Validate") {
-    sh "${MavenCommand}/bin/mvn clean validate"
+    sh "${Maven}/bin/mvn clean validate"
   }
   else if ("${MavenCommand}" == "Package") {
-    sh "${MavenCommand}/bin/mvn clean package"
+    sh "${Maven}/bin/mvn clean package"
   }
   else if ("${MavenCommand}" == "Install") {
-    sh "${MavenCommand}/bin/mvn clean install"
+    sh "${Maven}/bin/mvn clean install"
   }
   else if ("${MavenCommand}" == "Sonar") {
-    sh "${MavenCommand}/bin/mvn clean package sonar:sonar"
+    sh "${Maven}/bin/mvn clean package sonar:sonar"
   }
 }
